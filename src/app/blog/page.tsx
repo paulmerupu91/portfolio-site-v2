9965f92d7b2c740eab826d83cc4fccfadb5da730
unbound1-com <tech@unbound1.com>
2025-02-22 10:29:30 -0500

    Adds GraphQL query to the blog home page.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link'
 
 import { getBlogPostsFromApi } from '@/utils/index'
 
-async function page(): Promise<JSX.Element> {
+async function page(  ): Promise<JSX.Element> {
 
-    const blogPosts = await getBlogPostsFromApi()
-    // console.log('blogposts', blogPosts)
+    const blogPostsRes = await getBlogPostsFromApi();
+
+    const blogPosts = blogPostsRes?.posts?.edges || [];
+    console.log('blogposts', blogPosts);
     // const blogPosts = getBlogPostsFromApi( )
 
     return (
@@ -15,14 +17,50 @@ async function page(): Promise<JSX.Element> {
                 <h1 className=' text-3xl row-span-3 sticky top-0'>Blog</h1>
             </div>
             {
-                blogPosts?.map?.((post: any) => {
+                blogPosts?.map?.((edge: any) => {
+
+                    const post = edge.node;
+                    const {
+                        title,
+                        slug,
+                        excerpt,
+                        date,
+                        featuredImage = null
+                    } = post;
+
+                    const localTimeStr = new Date(date).toLocaleString();
+                    console.log( 'featuredImage', featuredImage );
+                    const { srcSet: srcSetFI = null, sourceUrl: sourceUrlFI = null } = featuredImage?.node || {};
+                    console.log( 'featuredImage.node', featuredImage?.node );
+                    console.log( 'srcSetFI', srcSetFI );
+                    console.log( 'sourceUrlFI', sourceUrlFI );
+                    const excerptTrimmed = trimHtml(excerpt, 100);
+
                     return (
-                        <Link key={post.id} href={`/blog/${post.slug}`} className='pb-6 border-b border-slate-200  dark:border-slate-700 dark:hover:bg-slate-900'>
+                        <Link key={post.id} href={`/blog/${slug}`} className='pb-6 border-b border-slate-200  dark:border-slate-700 dark:hover:bg-slate-900'>
                             {/* Post Image */}
                             {/* <img src={post.featured_media_data?.source_url} alt={post.title.rendered} className='w-full h-48 object-cover' /> */}
                             {/* Post Title */}
-                            <h2 className=' text-sky-700 mb-3 font-light text-2xl'>{post.title.rendered}</h2>
-                            <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></div>
+                            <div className="top-section mb-6">
+
+                                {/* Article Featured Image */}
+                                {
+                                    featuredImage &&
+                                    <div className='hero-img sm:mx-0'>
+                                        <img src={sourceUrlFI} alt={title} className='w-full aspect-3/2 object-cover' />
+                                    </div>
+                                }
+                            </div>
+                            <div className="bottom-section ">
+                                <h2 className=' text-sky-700 mb-3 font-light text-2xl'>{title}</h2>
+                                <div dangerouslySetInnerHTML={{ __html: excerptTrimmed }}></div>
+                                <time className=' text-slate-700 dark:text-slate-400 mt-2 block '
+                                    dateTime={date}
+                                >
+                                    {localTimeStr}
+                                </time>
+                            </div>
+
                         </Link>
                     )
                 })
@@ -32,3 +70,13 @@ async function page(): Promise<JSX.Element> {
 }
 
 export default page
+
+// Trim innerHTML to 100 characters
+function trimHtml(html: string, maxChars: number, elipsis = true): string {
+
+    const trimmed = html.substring(0, maxChars);
+    if (elipsis && html.length > maxChars) {
+        return `${trimmed}...`;
+    }
+    return trimmed;
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,69 @@
-// import gqlClient from './gqlClient';
+import { gql, request } from 'graphql-request';
 
-export function formatWpDateString( dateString : string ) {
-  const date = new Date( dateString );
+export function formatWpDateString(dateString: string) {
+    const date = new Date(dateString);
 
     // To local date string
-  return date.toLocaleDateString( 'en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    // timeZone: "EST",
-    // timeZoneName: 'short',
-  } );
+    return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        // timeZone: "EST",
+        // timeZoneName: 'short',
+    });
 }
 
 
 
-export function getBlogPostsFromApi() {
-    return fetch('https://blog-cms.paulmerupu.com/wp-json/wp/v2/posts', {
-            method: 'GET',
-            headers: {
-                'Cache-Control': 'no-store', // Prevents caching
-                Pragma: 'no-cache',          // HTTP/1.0 compatibility
+export async function getBlogPostsFromApi(): Promise<any> {
+    // return fetch('https://blog-cms.paulmerupu.com/wp-json/wp/v2/posts', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Cache-Control': 'no-store', // Prevents caching
+    //             Pragma: 'no-cache',          // HTTP/1.0 compatibility
+    //         }
+    //     })
+    //     .then(async response => await response.json())
+
+    const document = gql`
+        query {
+            posts {
+                edges {
+                node {
+                    title
+                    content
+                    slug
+                    excerpt
+                    terms {
+                    edges {
+                        node {
+                        slug
+                        name
+                        }
+                    }
+                    }
+                    date
+                    link
+                    modified
+                    databaseId
+                    featuredImage {
+                    node {
+                        id
+                        sourceUrl(size: LARGE)
+                        srcSet(size: LARGE)
+                        mediaItemUrl
+                        link
+                        altText
+                    }
+                    }
+                }
+                }
             }
-        })
-        .then(async response => await response.json())
+        }
+    `;
+
+    return await request('https://blog-cms.paulmerupu.com/graphql', document)
 }
 
